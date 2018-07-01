2c0de53a6d60c92fd1b34d76138ebd405b6d7af2
Jeffrey Polasz <jeff_polasz@hotmail.com>
2018-07-01 13:28:27 -0400

    Projectile movement components

--- Source/BattleTank/Private/Tank.cpp
+++ Source/BattleTank/Private/Tank.cpp
@@ -50,11 +50,10 @@ void ATank::AimAt(FVector OutHitLocation)
 
 void ATank::Fire()
 {
-	auto Time = GetWorld()->GetTimeSeconds();
-	UE_LOG(LogTemp, Warning, TEXT("%f: Tank fired"), Time);
-
 	if (!Barrel) { return; }
 
 	// Spawn a projectile at the socket location on the barrel
-	GetWorld()->SpawnActor<AProjectile>(ProjectileBluePrint, Barrel->GetSocketLocation(FName("Projectile")), Barrel->GetSocketRotation(FName("Projectile")));
+	auto Projectile = GetWorld()->SpawnActor<AProjectile>(ProjectileBluePrint, Barrel->GetSocketLocation(FName("Projectile")), Barrel->GetSocketRotation(FName("Projectile")));
+
+	Projectile->LaunchProjectile(LaunchSpeed);
 }
