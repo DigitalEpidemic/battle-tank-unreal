513b40848c683c3aed765f7a5221e96866aaed44
Jeffrey Polasz <jeff_polasz@hotmail.com>
2018-07-06 23:45:48 -0400

    Finalizing your class code and added copyright notice

--- Source/BattleTank/Private/TankMovementComponent.cpp
+++ Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright 2018 Jeffrey Polasz. All Rights Reserved.
 
 #include "TankMovementComponent.h"
 #include "TankTrack.h"
@@ -14,8 +14,6 @@ void UTankMovementComponent::IntendMoveForward(float Throw)
 	if (!LeftTrack || !RightTrack) { return; }
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(Throw);
-
-	// TODO Prevent double speed from dual control use
 }
 
 void UTankMovementComponent::IntendTurnRight(float Throw)
@@ -23,8 +21,6 @@ void UTankMovementComponent::IntendTurnRight(float Throw)
 	if (!LeftTrack || !RightTrack) { return; }
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(-Throw);
-
-	// TODO Prevent double speed from dual control use
 }
 
 void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed)
@@ -38,6 +34,4 @@ void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool
 
 	auto RightThrow = FVector::CrossProduct(TankForward, AIForwardIntention).Z;
 	IntendTurnRight(RightThrow);
-
-	//UE_LOG(LogTemp, Warning, TEXT("%s vectoring to %s"), *TankName, *MoveVelocityString)
 }
--- Source/BattleTank/Public/TankMovementComponent.h
+++ Source/BattleTank/Public/TankMovementComponent.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright 2018 Jeffrey Polasz. All Rights Reserved.
 
 #pragma once
 
@@ -26,10 +26,9 @@ public:
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void IntendTurnRight(float Throw);
 
-	// TODO Check best protection
+private:
 	virtual void RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed) override;
 
-private:
 	UTankTrack* LeftTrack = nullptr;
 	UTankTrack* RightTrack = nullptr;
 	
