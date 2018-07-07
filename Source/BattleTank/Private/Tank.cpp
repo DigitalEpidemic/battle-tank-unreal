be41aa2aacb5f816bdb971b3a421dfdbfa2278a5
Jeffrey Polasz <jeff_polasz@hotmail.com>
2018-07-07 02:02:23 -0400

    Decoupling your architecture

--- Source/BattleTank/Private/Tank.cpp
+++ Source/BattleTank/Private/Tank.cpp
@@ -13,19 +13,24 @@ ATank::ATank()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
+
+	auto TankName = GetName();
+	UE_LOG(LogTemp, Warning, TEXT ("%s DONKEY: Tank C++ Construct"), *TankName)
 }
 
 // Called when the game starts or when spawned
-//void ATank::BeginPlay()
-//{
-//	Super::BeginPlay();
-//	
-//}
+void ATank::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto TankName = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s DONKEY: Tank C++ Begin Play"), *TankName)
+}
 
 void ATank::AimAt(FVector OutHitLocation)
 {
+	if (!TankAimingComponent) { return; }
 	TankAimingComponent->AimAt(OutHitLocation, LaunchSpeed);
-	
 }
 
 void ATank::Fire()
--- Source/BattleTank/Public/Tank.h
+++ Source/BattleTank/Public/Tank.h
@@ -36,7 +36,7 @@ private:
 	ATank();
 
 	// Called when the game starts or when spawned
-	//virtual void BeginPlay() override;
+	virtual void BeginPlay() override;
 
 	UPROPERTY(EditDefaultsOnly, Category = "Setup")
 	TSubclassOf<AProjectile> ProjectileBluePrint;
