c11824a84b2d2cb0fac695dc5a3d0047f5b86431
Jeffrey Polasz <jeff_polasz@hotmail.com>
2018-07-09 16:04:22 -0400

    The observer pattern

--- Source/BattleTank/Private/TankAIController.cpp
+++ Source/BattleTank/Private/TankAIController.cpp
@@ -3,11 +3,30 @@
 #include "TankAIController.h"
 #include "TankAimingComponent.h"
 #include "Engine/World.h"
+#include "Tank.h" // To implement OnDeath
 // Depends on movement component via pathfinding system
 
 void ATankAIController::BeginPlay()
 {
 	Super::BeginPlay();
+}
+
+void ATankAIController::SetPawn(APawn* InPawn)
+{
+	Super::SetPawn(InPawn);
+	if (InPawn)
+	{
+		auto PossessedTank = Cast<ATank>(InPawn);
+		if (!ensure(PossessedTank)) { return; }
+
+		// Subscribe local method to Tank's death event
+		PossessedTank->OnDeath.AddUniqueDynamic(this, &ATankAIController::OnTankDeath);
+	}
+}
+
+void ATankAIController::OnTankDeath()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Received!"))
 }
 
 void ATankAIController::Tick(float DeltaTime)
